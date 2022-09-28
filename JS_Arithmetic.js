2c7fe88a4e10746b03b56073f97bebc8b4ff8ffd
Keketso Leu <keketsoleu12@gmail.com>
2022-09-28 18:26:06 +0200

    initial commit

---
+++ JS_Arithmetic.js
@@ -0,0 +1,41 @@
+// TITLE
+console.log(" ----- Area Of Shapes JS Arithmetic Challenge ----- \n\n")
+
+// Constant Variables:
+const pi = 3.1419;
+const half = 0.5;
+
+// Variables for measurements
+
+const recLength = 10;
+const recBreadth = 17;
+const circRadius = 25;
+const triBase = 20;
+const triHeight = 9;
+
+
+// Rectangle area calculation
+const recArea = recLength * recBreadth;
+
+// Append your rectangle answer:
+console.log("The area of the given rectangle is : " + recArea );
+
+
+console.log("\n");
+
+
+// circle area calculation
+const circleArea = Math.PI * Math.pow(circRadius, 2);
+
+// Append your circle answer:
+console.log("The area of the given circle is : " + circleArea);
+
+
+console.log("\n");
+
+
+// triangle area calculation
+const triangleArea = 0.5 * triBase * triHeight;
+
+// Append your triangle answer:
+console.log("The area of the given triangle is : " + triangleArea);
